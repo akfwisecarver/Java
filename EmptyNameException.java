9bd91767925d881e6f62dc1e4eccaa7c818a81b2
Alex Wisecarver <akfwisecarver2010@gmail.com>
2016-03-04 06:41:05 -0600

    Added files via upload

---
+++ EmptyNameException.java
@@ -0,0 +1,9 @@
+package Payroll;
+
+/**
+ * Created by Alex_Wisecarver on 3/3/2016.
+ */
+public class EmptyNameException extends Exception
+{
+
+}
---
+++ InvalidEmployeeIdException.java
@@ -0,0 +1,7 @@
+package Payroll;
+
+/**
+ * Created by Alex_Wisecarver on 3/3/2016.
+ */
+public class InvalidEmployeeIdException extends Exception {
+}
